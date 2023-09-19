5c5adb16ecb80d96bbc43889c3d0230b8d00f2a8
spinfal <public@spin.rip>
2023-09-19 12:26:28 +0000

    better customization

--- Global.example.js
+++ Global.example.js
@@ -1,6 +1,10 @@
 exports.config = {
-    port: 443,
-    host: 'localhost',
-    token: 'BOT TOKEN',
-    fileChannel: 'ID OF CHANNEL TO SEND FILES'
+  port: 443,
+  host: 'localhost',
+  token: 'BOT TOKEN',
+  fileChannel: 'ID OF CHANNEL TO SEND FILES',
+  maxFileSize: {
+    human: "100mb",
+    byte: 104857600
+  }
 }
