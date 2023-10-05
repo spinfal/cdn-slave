0c3601dcd8dcca1611198a39390f2bcb0489ed73
spinfal <public@spin.rip>
2023-10-05 15:53:10 +0000

    stop using dev tailwind
    moved to prod tailwind file -> index.css

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(fileUpload());
 /* static files */
 app.use(express.static('public'));
 // app.use('/js', express.static(__dirname + 'public/js'));
-// app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/css', express.static(__dirname + 'public/css'));
 // app.use('/imgs', express.static(__dirname + 'public/imgs'));
 
 /* set views */
